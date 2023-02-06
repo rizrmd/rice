b3d2bbbdde04c3aa2ca636253db11d8f9a02a836
Rizky <rizky05@gmail.com>
2023-02-06 08:47:55 +0700

    latest front build

--- core/front/build/index.b0a2d388.js
+++ core/front/build/index.b0a2d388.js
@@ -40139,7 +40139,7 @@ listenEvent(onMessage, async function(e) {
     }
 });
 
-},{"rpc":"jNaSg","server":"fwzIm","../state/app":"cFz1s","./w":"6jnvI","./wait-until":"2iPoQ","@parcel/transformer-js/src/esmodule-helpers.js":"beCOK","./app-action":"763eE"}],"jNaSg":[function(require,module,exports) {
+},{"rpc":"jNaSg","server":"fwzIm","../state/app":"cFz1s","./app-action":"763eE","./w":"6jnvI","./wait-until":"2iPoQ","@parcel/transformer-js/src/esmodule-helpers.js":"beCOK"}],"jNaSg":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "createBasicJsonRpcClient", ()=>(0, _createBasicJsonRpcClient.createBasicJsonRpcClient));
@@ -43984,7 +43984,7 @@ const createClient = (appName)=>{
     });
 };
 
-},{"cuid":"1NFTW","rpc":"jNaSg","./w":"6jnvI","@parcel/transformer-js/src/esmodule-helpers.js":"beCOK","../../../rice/src":"bcsV3"}],"1NFTW":[function(require,module,exports) {
+},{"cuid":"1NFTW","rpc":"jNaSg","./w":"6jnvI","../../../rice/src":"bcsV3","@parcel/transformer-js/src/esmodule-helpers.js":"beCOK"}],"1NFTW":[function(require,module,exports) {
 /**
  * cuid.js
  * Collision-resistant UID generator for browsers and node.
@@ -44070,13 +44070,15 @@ module.exports = getRandomValue;
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "cx", ()=>(0, _cx.cx));
+parcelHelpers.export(exports, "css", ()=>(0, _goober.css));
 parcelHelpers.export(exports, "createApp", ()=>createApp);
 parcelHelpers.export(exports, "app", ()=>app);
 parcelHelpers.export(exports, "readState", ()=>readState);
 parcelHelpers.export(exports, "bar", ()=>bar);
-parcelHelpers.export(exports, "frame", ()=>frame);
+parcelHelpers.export(exports, "importCSS", ()=>importCSS);
 var _deepProxy = require("@qiwi/deep-proxy");
 var _appAction = require("front/src/libs/app-action");
+var _goober = require("goober");
 var _cx = require("front/src/libs/cx");
 const createApp = (arg)=>{
     return arg;
@@ -44098,31 +44100,34 @@ const readState = (fn)=>{
             if (key === "___READ___") return path;
             return PROXY({});
         }));
-        const result = await app.rpc.read_state({
-            path: getter.___READ___
-        });
-        resolve(result);
+    // const result = await app.rpc.read_state({ path: getter.___READ___ });
+    // resolve(result);
     });
 };
 const bar = {
     create: async (fn)=>{
-        return await app.rpc.create_bar({
-            appName: app.name,
-            fn
-        });
-    }
-};
-const frame = {
-    create: async (arg)=>{
-        return await app.rpc.create_frame({
-            ...arg,
+        const barID = await app.rpc.createBarElement({
             appName: app.name
         });
-    },
-    close: async (arg)=>{}
+        const divEl = parent.window.document.getElementById(barID);
+        if (divEl) {
+            fn(divEl);
+            app.rpc.appendStyle("_goober", (0, _goober.extractCss)());
+        }
+    }
 };
+const importCSS = (appName, path)=>{
+    app.rpc.importCSS(appName, path);
+}; // export const frame = {
+ //   create: async (
+ //     arg: Omit<Parameters<typeof app.rpc.create_frame>[0], "appName">
+ //   ) => {
+ //     return await app.rpc.create_frame({ ...arg, appName: app.name });
+ //   },
+ //   close: async (arg: { windowID: string }) => {},
+ // };
 
-},{"@qiwi/deep-proxy":"g8MxE","front/src/libs/app-action":"763eE","front/src/libs/cx":"cO2cu","@parcel/transformer-js/src/esmodule-helpers.js":"beCOK"}],"g8MxE":[function(require,module,exports) {
+},{"@qiwi/deep-proxy":"g8MxE","front/src/libs/app-action":"763eE","goober":"936sQ","front/src/libs/cx":"cO2cu","@parcel/transformer-js/src/esmodule-helpers.js":"beCOK"}],"g8MxE":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "DEFAULT", ()=>o);
@@ -44256,6 +44261,95 @@ var t = {
 }, y = function(e, t, r, a) {
     return f(e, t, r, a);
 };
+
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"beCOK"}],"936sQ":[function(require,module,exports) {
+var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
+parcelHelpers.defineInteropFlag(exports);
+parcelHelpers.export(exports, "css", ()=>u);
+parcelHelpers.export(exports, "extractCss", ()=>r);
+parcelHelpers.export(exports, "glob", ()=>b);
+parcelHelpers.export(exports, "keyframes", ()=>h);
+parcelHelpers.export(exports, "setup", ()=>m);
+parcelHelpers.export(exports, "styled", ()=>j);
+let e = {
+    data: ""
+}, t = (t)=>"object" == typeof window ? ((t ? t.querySelector("#_goober") : window._goober) || Object.assign((t || document.head).appendChild(document.createElement("style")), {
+        innerHTML: " ",
+        id: "_goober"
+    })).firstChild : t || e, r = (e)=>{
+    let r = t(e), l = r.data;
+    return r.data = "", l;
+}, l = /(?:([\u0080-\uFFFF\w-%@]+) *:? *([^{;]+?);|([^;}{]*?) *{)|(}\s*)/g, a = /\/\*[^]*?\*\/|  +/g, n = /\n+/g, o = (e, t)=>{
+    let r = "", l = "", a = "";
+    for(let n in e){
+        let c = e[n];
+        "@" == n[0] ? "i" == n[1] ? r = n + " " + c + ";" : l += "f" == n[1] ? o(c, n) : n + "{" + o(c, "k" == n[1] ? "" : t) + "}" : "object" == typeof c ? l += o(c, t ? t.replace(/([^,])+/g, (e)=>n.replace(/(^:.*)|([^,])+/g, (t)=>/&/.test(t) ? t.replace(/&/g, e) : e ? e + " " + t : t)) : n) : null != c && (n = /^--/.test(n) ? n : n.replace(/[A-Z]/g, "-$&").toLowerCase(), a += o.p ? o.p(n, c) : n + ":" + c + ";");
+    }
+    return r + (t && a ? t + "{" + a + "}" : a) + l;
+}, c = {}, s = (e)=>{
+    if ("object" == typeof e) {
+        let t = "";
+        for(let r in e)t += r + s(e[r]);
+        return t;
+    }
+    return e;
+}, i = (e, t, r, i, p)=>{
+    let u = s(e), d = c[u] || (c[u] = ((e)=>{
+        let t = 0, r = 11;
+        for(; t < e.length;)r = 101 * r + e.charCodeAt(t++) >>> 0;
+        return "go" + r;
+    })(u));
+    if (!c[d]) {
+        let t = u !== e ? e : ((e)=>{
+            let t, r, o = [
+                {}
+            ];
+            for(; t = l.exec(e.replace(a, ""));)t[4] ? o.shift() : t[3] ? (r = t[3].replace(n, " ").trim(), o.unshift(o[0][r] = o[0][r] || {})) : o[0][t[1]] = t[2].replace(n, " ").trim();
+            return o[0];
+        })(e);
+        c[d] = o(p ? {
+            ["@keyframes " + d]: t
+        } : t, r ? "" : "." + d);
+    }
+    let f = r && c.g ? c.g : null;
+    return r && (c.g = c[d]), ((e, t, r, l)=>{
+        l ? t.data = t.data.replace(l, e) : -1 === t.data.indexOf(e) && (t.data = r ? e + t.data : t.data + e);
+    })(c[d], t, i, f), d;
+}, p = (e, t, r)=>e.reduce((e, l, a)=>{
+        let n = t[a];
+        if (n && n.call) {
+            let e = n(r), t = e && e.props && e.props.className || /^go/.test(e) && e;
+            n = t ? "." + t : e && "object" == typeof e ? e.props ? "" : o(e, "") : !1 === e ? "" : e;
+        }
+        return e + l + (null == n ? "" : n);
+    }, "");
+function u(e) {
+    let r = this || {}, l = e.call ? e(r.p) : e;
+    return i(l.unshift ? l.raw ? p(l, [].slice.call(arguments, 1), r.p) : l.reduce((e, t)=>Object.assign(e, t && t.call ? t(r.p) : t), {}) : l, t(r.target), r.g, r.o, r.k);
+}
+let d, f, g, b = u.bind({
+    g: 1
+}), h = u.bind({
+    k: 1
+});
+function m(e, t, r, l) {
+    o.p = t, d = e, f = r, g = l;
+}
+function j(e, t) {
+    let r = this || {};
+    return function() {
+        let l = arguments;
+        function a(n, o) {
+            let c = Object.assign({}, n), s = c.className || a.className;
+            r.p = Object.assign({
+                theme: f && f()
+            }, c), r.o = / *go\d+/.test(s), c.className = u.apply(r, l) + (s ? " " + s : ""), t && (c.ref = o);
+            let i = e;
+            return e[0] && (i = c.as || e, delete c.as), g && i[0] && g(c), d(i, c);
+        }
+        return t ? t(a) : a;
+    };
+}
 
 },{"@parcel/transformer-js/src/esmodule-helpers.js":"beCOK"}]},["3kpDH","5d35h","4aBH6"], "4aBH6", "parcelRequire1ea1")
 
